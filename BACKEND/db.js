bfe352b758a18f7f196922229147a8b0a840c8de
PASULA NIRANJAN KUMAR <24p61a05r3@vbithyd.ac.in>
2026-05-05 22:52:19 +0530

    Refactor DB connection to use environment variables
    Updated database connection configuration to use environment variables.

--- BACKEND/db.js
+++ BACKEND/db.js
@@ -1,12 +1,13 @@
 console.log("Using Railway DB...");
+
 const mysql = require("mysql2");
 
 const db = mysql.createConnection({
-    host: "roundhouse.proxy.rlwy.net",
-    user: "root",
-    password: "qswxgDRWJedHxrovHRjUfwwplLGfajUQ",
-    database: "railway",
-    port: 44292
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT
 });
 
 // CONNECT TEST
